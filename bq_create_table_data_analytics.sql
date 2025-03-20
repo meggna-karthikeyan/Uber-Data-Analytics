4639785e6dfbcd9d59ad5d0e473a97fcc02f93eb
Meggna Karthikeyan <kmeggna@gmail.com>
2025-03-20 20:13:44 +0530

    Add files via upload

---
+++ bq_create_table_data_analytics.sql
@@ -0,0 +1,20 @@
+CREATE TABLE `data-with-meggna-452321.uber_data_engineering_project_meggna.data_analytics`
+(
+  datetime_id INT64,
+  VendorID INT64,
+  tpep_pickup_datetime STRING,
+  tpep_dropoff_datetime STRING,
+  passenger_count FLOAT64,
+  trip_distance FLOAT64,
+  rate_code_name STRING,
+  PULocationID INT64,
+  DOLocationID INT64,
+  payment_type_name STRING,
+  fare_amount FLOAT64,
+  extra FLOAT64,
+  mta_tax FLOAT64,
+  tip_amount FLOAT64,
+  tolls_amount FLOAT64,
+  improvement_surcharge FLOAT64,
+  total_amount FLOAT64
+);
